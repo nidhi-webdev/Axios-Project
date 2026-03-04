8935cb306a24388fe8155799a1f9a8d12d83a2ec
nidhi <kumarinidhi0594@gmail.com>
2026-03-04 16:40:51 +0100

    Getting the data from Axios

--- Axios-project/src/pages/Movie.jsx
+++ Axios-project/src/pages/Movie.jsx
@@ -1,4 +1,23 @@
+import { useEffect } from "react"
+import axios from 'axios'
+
 export const Movie = () => {
-    const api = "http://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1"
+    const API = "http://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1"
+
+    const getMovieData = async () => {
+        try {
+            const res = await axios.get(API)
+            console.log("This is from Axios File", res)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    useEffect(() => {
+        getMovieData()
+    }, [])
+
+
     return <h1> Movie </h1>
 }
