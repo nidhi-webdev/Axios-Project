4f6824c64dca2ea12aa891a99b2bd18debbd199d
nidhi <kumarinidhi0594@gmail.com>
2026-03-04 17:08:46 +0100

    PAssing the data to card component

--- Axios-project/src/pages/Movie.jsx
+++ Axios-project/src/pages/Movie.jsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from 'axios'
 
 export const Movie = () => {
     const API = "http://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1"
+    const [data, setData] = useState([])
 
     const getMovieData = async () => {
         try {
             const res = await axios.get(API)
             console.log("This is from Axios File", res)
+            setData(res.data.Search)
         } catch (error) {
             console.log(error);
         }
@@ -19,5 +21,16 @@ export const Movie = () => {
     }, [])
 
 
-    return <h1> Movie </h1>
+    return (
+        <div>
+            <ul>
+                <li>
+                    {data.map((curelem) => {
+                        return <Card key={imdbID} curelem={curelem}  />
+                    } )}
+                </li>
+            </ul>
+            
+        </div>
+    )
 }
