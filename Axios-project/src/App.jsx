f970f5afb5264fc8b2183894a66a9e1adc13ac63
nidhi <kumarinidhi0594@gmail.com>
2026-03-05 16:59:16 +0100

    getting the data using useeffect

--- Axios-project/src/App.jsx
+++ Axios-project/src/App.jsx
@@ -1,9 +1,23 @@
 import { Movie } from "./pages/Movie"
+import { getApi } from "./api/Crud"
+import { useEffect } from "react";  
 
 const App = () => {
+  console.log(getApi());
+
+  const getCurdapi = async () => {
+    const data = await getApi()
+    console.log("This is from Crud file", data)
+  }
+
+  useEffect(() => {
+    getCurdapi()
+  }, [])
+
   return (
     <div>
-      <Movie />
+      {/* <Movie /> */}
+
     </div>
   )
 }
