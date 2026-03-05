7206c5b247bee2ed8061ef066419658667f68235
nidhi <kumarinidhi0594@gmail.com>
2026-03-05 16:34:07 +0100

    The real recommemned way

--- Axios-project/src/pages/Movie.jsx
+++ Axios-project/src/pages/Movie.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react"
-import axios from 'axios'
+import { getMovie } from '../services/GetServices'
 import Card from "../components/Card"
 
 export const Movie = () => {
-    const API = "http://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1"
+    
     const [data, setData] = useState([])
 
     const getMovieData = async () => {
         try {
-            const res = await axios.get(API)
+            const res = await getMovie()
             setData(res.data.Search)
         } catch (error) {
             console.log("Error message:", error);
